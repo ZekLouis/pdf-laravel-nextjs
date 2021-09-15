9f44be2f9e6d6af71a2ff5e48589591ef6231f77
MARGAUX PRECLOUX <margaux@iconosqua.re>
2021-09-15 14:56:18 +0200

    remove responsive grid

--- nextjs/pages/printgrid.jsx
+++ nextjs/pages/printgrid.jsx
@@ -4,13 +4,13 @@ import KPI from '../src/KPI';
 export default function PrintGrid() {
     return (
         <div className="flex flex-col w-full">
-            <div className="w-full grid grid-cols-12 gap-y-8 lg:gap-y-12 gap-x-2 xl:gap-x-m 2xl:gap-x-5 break-after">
+            <div className="w-full grid grid-cols-12 gap-y-8 gap-x-2 break-after">
                 <Chart className="col-span-full" />
                 <KPI className="col-span-full sm:col-span-6 lg:col-span-4" />
                 <KPI className="col-span-full sm:col-span-6 lg:col-span-4" />
                 <KPI className="col-span-full sm:col-span-6 lg:col-span-4" />
             </div>
-            <div className="w-full grid grid-cols-12 gap-y-8 lg:gap-y-12 gap-x-2 xl:gap-x-m 2xl:gap-x-5 break-after">
+            <div className="w-full grid grid-cols-12 gap-y-8 gap-x-2 break-after">
                 <Chart className="col-span-full" />
                 <KPI className="col-span-full sm:col-span-6 lg:col-span-4" />
                 <KPI className="col-span-full sm:col-span-6 lg:col-span-4" />
